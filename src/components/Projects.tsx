c20e47481892004ed6628703610ef244846b045e
Samuel Pressé-Lefebvre <samuelpresse@gmail.com>
2023-12-29 21:38:30 -0500

    Add motion to projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,24 +1,56 @@
 import Heading from "./Heading";
 import projects from "../content/projects";
 import ProjectCard from "./ProjectCard";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 const Projects = () => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView({ triggerOnce: true });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
+  const containerVariants = {
+    hidden: {},
+    visible: {
+      transition: {
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
+  const projectVariants = {
+    hidden: { opacity: 0, x: -20 },
+    visible: { opacity: 1, x: 0 },
+  };
+
   return (
     <>
       <Heading firstWord="My" secondWord="Projects" />
 
-      <div className="project-map">
+      <motion.div
+        className="project-map"
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={containerVariants}
+      >
         {projects.map((project, index) => (
-          <div key={index}>
+          <motion.div key={index} variants={projectVariants}>
             <ProjectCard
               name={project.name}
               img={project.img}
               description={project.description}
               source={project.sourceCode}
             />
-          </div>
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
     </>
   );
 };
